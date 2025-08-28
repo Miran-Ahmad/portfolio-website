b528080fa1840cf8fc64874d6b26044e4672bb54
Miran Ahmad <222miran222@gmail.com>
2025-08-28 15:57:05 +0530

    Update

--- src/app/components/ui/HeroSection.tsx
+++ src/app/components/ui/HeroSection.tsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
     >
       <div className="space-y-1">
         <h2 className="z-10 text-3xl font-bold text-center text-transparent duration-1000 bg-white cursor-default text-stroke animate-title sm:text-5xl md:text-6xl whitespace-nowrap bg-clip-text pb-10">
-          Hey, I'm
+          Hey, I&apos;m
         </h2>
         <h1 className="z-10 text-4xl font-bold text-center text-transparent duration-1000 bg-white cursor-default sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text bg-gradient-to-r from-purple-300 to-purple-800 animate-fade-in-3">
           Miran
